55dc92158b1e0b719fdac18a8be2827394bb2de1
deadshot674gam <sunnyt0225@gmail.com>
2021-03-04 16:32:10 +0530

    added express

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 const geocode = require('./geocode')
 const starmap = require('./starmap')
+const express = require('express')
+const path = require('path')
+const e = require('express')
+const app = express()
 
-geocode("Kanpur",starmap)
+const staticpath = path.join(__dirname,'../static')
+
+app.use(express.static(staticpath))
+app.get('',(req,res)=>{
+
+})
+
+app.listen(5000,()=>{
+    console.log('Server running in port 5000')
+})
+// geocode("Kanpur",starmap)
